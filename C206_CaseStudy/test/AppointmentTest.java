50c5cc4a478313d6782a1416923292c47e1fc41c
19046078 <19046078@DESKTOP-519GUOB>
2020-08-25 14:26:08 +0800

    appointment done

--- C206_CaseStudy/test/AppointmentTest.java
+++ C206_CaseStudy/test/AppointmentTest.java
@@ -57,5 +57,31 @@ public class AppointmentTest {
 				assertEquals("Test that second item in appointmentlist is the same", ap2, AppointmentArray.appointmentList.get(1));
 		 
 	}
+	@Test
+	public void viewAllappointments() {
+
+		// Test that appointmentlist is not null but empty
+		assertNotNull("Test that appointmentlist is not null", AppointmentArray.appointmentList);
+		// Test if the  appointmentlist is empty
+		String testOutput = "No appointment Found";
+		String viewappointment = AppointmentArray.retrieveAllappointments(AppointmentArray.appointmentList);
+		assertEquals("Test if the appointmentlist is empty", testOutput,viewappointment );
+
+		//Test that viewAllappointments() method returns a String that equals to expectedOutput2
+		AppointmentArray.addappointments(AppointmentArray.appointmentList, ap1);
+		AppointmentArray.addappointments(AppointmentArray.appointmentList, ap2);
+		
+		String output = String.format("%-20s %-10s\n", "APPOINTMENT ID", "APPOINTMENT DATE");
+		
+		for(int i = 0; i < AppointmentArray.appointmentList.size(); i++ ) {
+			output += String.format("%-20d %d/%d/%d", AppointmentArray.appointmentList.get(i).getdateid(),
+					AppointmentArray.appointmentList.get(i).getDay(), AppointmentArray.appointmentList.get(i).getMonth(), AppointmentArray.appointmentList.get(i).getYear());
+				
+		}
+		String viewappointment2 = AppointmentArray.retrieveAllappointments(AppointmentArray.appointmentList);
+		assertEquals("Test that viewAllCategory() method return a String that equals expectedOutput2",output, viewappointment2);
+		
+	}
+
 
 }
